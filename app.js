b3553bd5d60d257da872a87cbdc56941c87bb10b
Ksenia Troitskaja <xeniacserkun@gmail.com>
2020-03-13 16:24:35 +0100

    New console.log

--- app.js
+++ app.js
@@ -131,6 +131,9 @@ var budgetController = ( function () {
                 data.allItems[type].splice(index, 1);   // SPLICE METHOD  for deleting
                 
             }
+
+
+            console.log(ids)
         },
 
 
